0573bcfd424f2d5e911407882397ff8a16384357
Oleh Shchyhol <shchyhol@gmil.com>
2022-06-05 23:06:39 +0100

    Third commite

--- script.js
+++ script.js
@@ -2,6 +2,4 @@ var app = document.getElementById("app");
 var p = document.createElement("p");
 var h1 = document.createElement("h1");
 p.textContent = "Hello World";
-h1.textContent = "Yeva";
 app === null || app === void 0 ? void 0 : app.appendChild(p);
-app === null || app === void 0 ? void 0 : app.appendChild(h1);
--- script.ts
+++ script.ts
@@ -3,6 +3,5 @@ const app = document.getElementById("app");
 const p = document.createElement("p");
 const h1 = document.createElement("h1");
 p.textContent = "Hello World";
-h1.textContent = "Yeva";
+
 app?.appendChild(p);
-app?.appendChild(h1);
